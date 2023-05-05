c2600488d4fd9ff2c9dd029916b43287deedfa93
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-05-05 00:19:14 -0500

    added all the routes that are needed

--- server.js
+++ server.js
@@ -1,19 +1,57 @@
 const express = require('express');
 const fs = require('fs');
-const uuid = require('./helpers/uuid');
 const path = require('path');
-//
 
 const PORT = 3001;
 
 const app = express();
 
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
-// GET Route for homepage
+
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html')),
 );
+
+
+app.get('/api/notes', (req, res) =>
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+    console.error(err)
+    } else {
+     return console.log(data),
+     res.json(JSON.parse(data))
+    }
+}));
+
+
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add new note`);
+  const { title, text } = req.body;
+  if (title && text) {
+    const newnote = {
+      title,
+      text,
+      note_id: Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+    };
+
+    readAndAppend(newnote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newnote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error creating new note');
+  }
+});
+
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
